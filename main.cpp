4ef4cd5481c77cdd1c38a062da28e4f418125727
♡ gabby ♡ <81900622+GabbyByers@users.noreply.github.com>
2023-12-08 17:27:36 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,20 +5,30 @@
 #include "SFML/Graphics.hpp"
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Hello SFML", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(700, 700), "Hello SFML", sf::Style::Close);
 	sf::Event event;
+	ImGui::SFML::Init(window);
+	ImGui::GetIO().IniFilename = NULL;
 
+	sf::Clock deltaClock;
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-			{
+			ImGui::SFML::ProcessEvent(event);
+			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 		}
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("my pretty window!");
+		ImGui::Text("some text!");
+		ImGui::End();
 
 		window.clear(sf::Color(0, 0, 0));
+		ImGui::SFML::Render(window);
 		window.display();
 	}
 
+	ImGui::SFML::Shutdown();
 	return 0;
 }
